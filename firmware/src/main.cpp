8604e24e0617ea3a5b10186537bdad44c2a72117
jandadav <janda.david@gmail.com>
2020-05-13 20:34:05 +0200

    basic loop

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -15,8 +15,8 @@ TimeHandler timeHandler;
 Relays relays;
 Distance distance;
 
-// enum SystemState {active, idle};
-// SystemState activeState = idle;
+enum SystemState {active, idle};
+SystemState activeState = idle;
 
 boolean pumpOn = false;
 
@@ -37,17 +37,17 @@ void setup(void) {
 
   webServerAgent.commandHandler.addCommandCallback("dummy", [](String c) { return (String) ("dummy command handler receiving: "+c);});
   webServerAgent.commandHandler.addCommandCallback("time", [](String c) {char time[20]; timeHandler.getTime(time); return (String)(time); });
-  webServerAgent.commandHandler.addCommandCallback("pump", [](String c) { 
-    if (pumpOn) {
-      pumpOn = false;
-    } else {
-      pumpOn = true;
-    }
-    return (String)("inverting pump");
-  });
+  // webServerAgent.commandHandler.addCommandCallback("pump", [](String c) { 
+  //   if (pumpOn) {
+  //     pumpOn = false;
+  //   } else {
+  //     pumpOn = true;
+  //   }
+  //   return (String)("inverting pump");
+  // });
   webServerAgent.commandHandler.addCommandCallback("waterLevel", [](String c) {return String(distance.getDistanceCm());});
-  // webServerAgent.commandHandler.addCommandCallback("start", [](String c) {activeState = active; LOG.verbose("cycle start"); return String("cycle start");});
-  // webServerAgent.commandHandler.addCommandCallback("stop", [](String c) {activeState = idle; LOG.verbose("cycle stop"); return String("cycle stop");});
+  webServerAgent.commandHandler.addCommandCallback("start", [](String c) {activeState = active; LOG.verbose("cycle start"); return String("cycle start");});
+  webServerAgent.commandHandler.addCommandCallback("stop", [](String c) {activeState = idle; LOG.verbose("cycle stop"); return String("cycle stop");});
 
   relays.setup();
 
@@ -60,21 +60,25 @@ void setup(void) {
 
 void loop(void) {
 
-  if (pumpOn) {
-    relays.pumpOn();
-  } else {
-    relays.pumpOff();
-  }
-
-  // if(activeState == active) {
-  //   if (distance.getDistanceCm() < 20) {
-  //     activeState = idle;
-  //   }
+  // if (pumpOn) {
   //   relays.pumpOn();
   // } else {
   //   relays.pumpOff();
   // }
 
+  if(activeState == active) {
+    if (distance.getDistanceCm() < 20) {
+      activeState = idle;
+    }
+    relays.pumpOn();
+  } else {
+    relays.pumpOff();
+       if (distance.getDistanceCm() > 20) {
+    activeState = active;
+  }
+  }
+
+  
 
   OtaUpdate();
   timeHandler.update();
